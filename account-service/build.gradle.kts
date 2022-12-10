1d7f01c60f363f10ef21f60307a10c4fe528d931
도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>
2022-12-10 17:52:50 +0900

    :rocket: Update deployment related stuff

--- account-service/build.gradle.kts
+++ account-service/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     kotlin("plugin.spring")
 }
 
+tasks.named("jar") {
+    enabled = false
+}
+
 dependencies {
     implementation(project(":account-role"))
     implementation(Libs.SPRING_BOOT_STARTER_WEBFLUX)
--- data-query-service/src/test/kotlin/com/samsung/healthcare/dataqueryservice/DataQueryServiceApplicationTests.kt
+++ data-query-service/src/test/kotlin/com/samsung/healthcare/dataqueryservice/DataQueryServiceApplicationTests.kt
@@ -1,6 +1,5 @@
 package com.samsung.healthcare.dataqueryservice
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
